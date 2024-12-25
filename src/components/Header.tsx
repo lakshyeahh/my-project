446c64bf1050def5957b9a022d769cbe45bcc4af
lakshyeahh <verma.lakshya071@gmail.com>
2024-12-25 17:29:20 +0530

    fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,1 @@
-import React from 'react'
 
-
-export default function Header() {
-  const [currentTime, setCurrentTime] = React.useState("9:41")
-
-  return (
-    <div className="flex justify-between items-center px-5 py-2 text-sm font-medium">
-      <span>{currentTime}</span>
-      <div className="flex gap-1">
-        <span className="font-bold">●●●●</span>
-        <span>WiFi</span>
-        <span>100%</span>
-      </div>
-    </div>
-  )
-} 
